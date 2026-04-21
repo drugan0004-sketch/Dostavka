7bd0aeb0b2d5a6e6cc81399a4818eb6bc1c54aca
drugan0004-sketch <drugan0004@gmail.com>
2026-04-21 09:09:37 +0300

    Delete First/First.cpp

--- First/First.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout << "Привет, мир! Это моя первая программа на C++." << std::endl;
-    int a = 5, b = 10;
-    int sum = a + b;
-    std::cout << "Сумма " << a << " и " << b << " равна " << sum << std::endl;
-    return 0;
-}

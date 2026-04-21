6de7818f6cc8d06ef81307310a065e4c49bf0c8e
vit77all-eng <vit77all@gmail.com>
2026-04-21 10:44:04 +0400

    Update project.cpp
    Добавлен различный интерфейс входа

--- project.cpp
+++ project.cpp
@@ -1,86 +1,55 @@
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 #include<windows.h>
 #include <string>
 #include <registr.h>
+#include"vhod.cpp"
 using namespace std;
-
-std::string get_log() {
-    std::string log;
-    std::cout << "Введите логин:" << std::endl;
-    std::cin >> log;
-    //std::getline(std::cin, log);
-    return log;
-}
-std::string get_pass() {
-    std::string pass;
-    std::cout << "Введите пароль:" << std::endl;
-    std::cin >> pass;
-    //std::getline(std::cin, pass);
-    return pass;
-}
-void find_in_menu() {
-    
-    ifstream file("каталог.txt");
-
-    if (!file.is_open()) {
-        return;
-    }
-
-    vector<string> katalog;
-    string line;
-
-    while (getline(file, line)) {
-        katalog.push_back(line);
-    }
-    file.close();
-
-    cout << "поиск: ";
-    string query;
-    cin >> query;
-    //getline(cin, query); не работало
-
-    cout << "\n Результаты:" << endl;
-
-    for (int i = 0; i < katalog.size(); i++) {
-        if (katalog[i].find(query) != string::npos) {
-            cout << "- " << katalog[i] << endl;
-        }
-    }
-}
 int main()
 {
     int select = 0;
     string login;
     string password;
-    bool running = true;
+    bool running_log = true;
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    while (running) {
-        cout << endl;
+    while (running_log) {
+        cout<<" Здревствуйте, гость. " << endl;
         cout << "Выберите действие: " << endl;
         cout << "0 - Выход " << endl;
         cout << "1 - Регистрация " << endl;
-        //cout << "2 - Вход в аккаунт" << endl;
-        cout << "2- Поиск " << endl;
+        cout << "2 - Вход в аккаунт" << endl;
+        //cout << "3-: " << endl;
         //cout << "4-: " << endl;
         cin >> select;
         switch (select)
         {
         case 0:
-            running = false;
-            break;
-
-        case 1:
-            login = get_log();
-            password = get_pass();
+            running_log = false;
             break;
         
         case 1:
             login = get_log();
             password = get_pass();
+            ofstream fout;
+            fout.open("logins.txt", ios::out);
+            fout<<login<<endl<<password<<endl;
             break;
         }
+        case 2:
+            if(IsLogin(login,password)){
+                
+            running_log = false;}
+            break;
+
+    }
+    While(!running_log)
+    {
+        cout<<" Здревствуйте,"<< login << endl;
+        cout << "Выберите действие: " << endl;
+        cin >> select;
+        
     }
     return 0;
-
+   
 }

1a57b4ea8808bb2df96dc0f09f450ac33ba7f639
vit77all-eng <vit77all@gmail.com>
2026-04-27 22:46:23 +0400

    Update project.cpp
    Добавлена функция входа.

--- project.cpp
+++ project.cpp
@@ -1,55 +1,91 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
-#include<windows.h>
+#include <windows.h>
 #include <string>
-#include <registr.h>
-#include"vhod.cpp"
+#include <clocale>
+#include "header.h"
+
 using namespace std;
 int main()
 {
+    system("chcp 65001 > nul");
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
     int select = 0;
     string login;
     string password;
+    string address;
     bool running_log = true;
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    while (running_log) {
-        cout<<" Здревствуйте, гость. " << endl;
+    
+
+    while (running_log == true) {
+        cout << "Здравствуйте, Гость" << endl;
         cout << "Выберите действие: " << endl;
         cout << "0 - Выход " << endl;
         cout << "1 - Регистрация " << endl;
         cout << "2 - Вход в аккаунт" << endl;
-        //cout << "3-: " << endl;
-        //cout << "4-: " << endl;
         cin >> select;
+
         switch (select)
         {
         case 0:
             running_log = false;
             break;
-        
-        case 1:
+
+        case 1: {
+            cin.ignore(); //после ввода select остаётся \n, который игнорируется этим
             login = get_log();
             password = get_pass();
-            ofstream fout;
-            fout.open("logins.txt", ios::out);
-            fout<<login<<endl<<password<<endl;
+            address = get_address();
+            ofstream fout("users.txt", ios::out);
+
+            if (!fout.is_open()) {
+                cout << "Файл не открыт" << endl;
+                break;
+            }
+            fout << login << endl << password << endl << address << endl;
+            fout.close();
             break;
         }
+        
         case 2:
-            if(IsLogin(login,password)){
-                
-            running_log = false;}
+            cout << "Введите логин: ";
+            cin >> login;
+            cout << "Введите пароль: ";
+            cin >> password;
+            if (IsLogin(login, password) == true) {
+                cout << "Пользователь найден" << endl;
+                running_log = false;
+            }
+            else {
+                cout << "Пользователь не найден" << endl;
+            }
+            break;
+        }
+    }
+
+    while (!running_log)
+    {
+        cout << "Здравствуйте, " << login << endl;
+        cout << "Выберите действие: " << endl;
+        cout << "0 - Выход " << endl;
+        //cout << "1 - Регистрация " << endl;
+        //cout << "2 - Вход в аккаунт" << endl;
+        cin >> select;
+        switch (select)
+        {
+        case 0:
+            running_log = true;
             break;
 
+        /*case 1:
+            search();
+            break;
+        */
+        }
     }
-    While(!running_log)
-    {
-        cout<<" Здревствуйте,"<< login << endl;
-        cout << "Выберите действие: " << endl;
-        cin >> select;
-        
-    }
+
     return 0;
-   
+
 }

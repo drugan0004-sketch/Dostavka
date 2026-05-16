4616abaa175740572566178644fcbdbade2c7604
vit77all-eng <vit77all@gmail.com>
2026-05-16 14:47:10 +0400

    Update vhod.cpp

--- vhod.cpp
+++ vhod.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <registr.h>
 #include <string>
+#include <io.h>
+#include <fcntl.h>
 using namespace std;
+
+void printlog() {
+    cout << "Здравствуйте, Гость" << endl;
+    cout << "Выберите действие: " << endl;
+    cout << "0 - Выход " << endl;
+    cout << "1 - Регистрация " << endl;
+    cout << "2 - Вход в аккаунт" << endl;
+};
+void printrun() {
+    system("chcp 65001 > nul");
+    cout << "Выберите действие: " << endl;
+    cout << "0 - Выход " << endl;
+    cout << "1 - Вернутся на главную " << endl;
+    cout << "2 - Вернутся на  страницу входа" << endl;
+};
+
 string get_log() {
     string log;
     cout << "Введите логин:" << endl;
+    //print("Введите логин:");
     getline(cin, log);
     return log;
 }
@@ -24,10 +42,12 @@ bool IsLogin(string login, string password) {
     while (getline(file, fLogin) && getline(file, fPassword)) {
         if (fLogin == login && fPassword == password) {
             file.close();
+            
             return true; // Нашли совпадение
         }
     }
 
     file.close();
+    cout << "Пользователь не найден" << endl;
     return false; // Совпадений не найдено
 }

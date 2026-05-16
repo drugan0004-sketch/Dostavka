60c82bb12c5a7d3e5c97287ad698889fe4f5ee86
vit77all-eng <vit77all@gmail.com>
2026-05-16 14:50:52 +0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -3,8 +3,7 @@
 #include <windows.h>
 #include <string>
 #include <clocale>
-#include "header.h"
-#include "vhod.cpp"
+#include "login.cpp"
 #include "search.cpp"
 using namespace std;
 int main()
@@ -13,48 +12,46 @@ int main()
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
+
     int select = 0;
     string login;
     string password;
-    string address;
     bool running_log = true;
     
-
+    start1:
     while (running_log == true) {
-        cout << "Здравствуйте, Гость" << endl;
-        cout << "Выберите действие: " << endl;
-        cout << "0 - Выход " << endl;
-        cout << "1 - Регистрация " << endl;
-        cout << "2 - Вход в аккаунт" << endl;
+        printlog();
+
         cin >> select;
 
         switch (select)
         {
         case 0:
             running_log = false;
+            exit(1);
             break;
 
         case 1: {
+
             cin.ignore(); //после ввода select остаётся \n, который игнорируется этим
             login = get_log();
             password = get_pass();
-            address = get_address();
             ofstream fout("users.txt", ios::out);
 
             if (!fout.is_open()) {
                 cout << "Файл не открыт" << endl;
                 break;
             }
-            fout << login << endl << password << endl << address << endl;
+            fout << login << endl << password << endl;//<<// address << endl;
             fout.close();
+            login.clear(); password.clear();
             break;
         }
         
         case 2:
-            cout << "Введите логин: ";
-            cin >> login;
-            cout << "Введите пароль: ";
-            cin >> password;
+            cin.ignore();
+            login = get_log();
+            password = get_pass();
             if (IsLogin(login, password) == true) {
                 cout << "Пользователь найден" << endl;
                 running_log = false;
@@ -62,29 +59,32 @@ int main()
             else {
                 cout << "Пользователь не найден" << endl;
             }
+            
             break;
         }
     }
 
     while (!running_log)
     {
-        cout << "Здравствуйте, " << login << endl;
-        cout << "Выберите действие: " << endl;
-        cout << "0 - Выход " << endl;
-        cout << "1 - Поиск " << endl;
-        //cout << "2 - Вход в аккаунт" << endl;
+        start2:
+        search();
+        printrun();
         cin >> select;
-        switch (select)
-        {
-        case 0:
-            running_log = true;
-            break;
+        switch (select) {
+            case 0:
+                exit(1);
+                break;
+            case 1 :
+                goto start2;
+                break;
+            case 2:
+                running_log = 1;
+                goto start1;
+                break;
+    }
 
-        case 1:
-            search();
-            break;
-        
-        }
+
+
     }
 
     return 0;
